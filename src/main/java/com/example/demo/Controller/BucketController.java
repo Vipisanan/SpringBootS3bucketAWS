0d2d674e4da5f0bb4dbc5f4d5373c17a6e3ba128
Vipisanan <vipisanan1@gmail.com>
2019-05-25 16:53:02 +0530

    it can upload file to s3

---
+++ src/main/java/com/example/demo/Controller/BucketController.java
@@ -0,0 +1,23 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("bucket")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("upload")
+    public String upload(@RequestPart(value = "file")MultipartFile multipartFile){
+        return this.amazonClient.uploadFile(multipartFile);
+    }
+}
---
+++ src/main/java/com/example/demo/Service/AmazonClient.java
@@ -0,0 +1,71 @@
+package com.example.demo.Service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Service
+public class AmazonClient {
+
+
+    private AmazonS3 s3client;
+
+    @Value("${endpointUrl}")
+    private String endpointUrl;
+    @Value("${jsa.s3.bucket}")
+    private String bucketName;
+    @Value("${jsa.aws.access_key_id}")
+    private String accessKey;
+    @Value("${jsa.aws.secret_access_key}")
+    private String secretKey;
+
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.s3client = new AmazonS3Client(credentials);
+    }
+
+    public String uploadFile(MultipartFile multipartFile){
+        String fileUrl = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+            return fileUrl;
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName , fileName , file));
+    }
+
+    private String generateFileName(MultipartFile multipartFile) {
+        return new Date().getTime() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
+    }
+
+    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
+        File conFile = new File(multipartFile.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(conFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return conFile;
+
+    }
+
+
+}
